c90118a4ba4a24c7264bb18b59b221da924d3c8e
Achal47568 <114794524+Achal47568@users.noreply.github.com>
2026-08-10 21:56:30 +0530

    Time: 53 ms (9.95%) | Memory: 31.3 MB (73.79%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,22 +1,28 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        current = nums[0]
-        maximum = nums[0]
+        maxSum = nums[0]
+        currentSum = nums[0]
 
         for i in range(1, len(nums)):
-            current = max(nums[i], current+nums[i])
-            maximum = max(maximum, current)
-        return maximum
+            currentSum = max(nums[i], currentSum + nums[i])
+            maxSum = max(maxSum, currentSum)
+
+        return maxSum
+
 
 """
-nums = [-2,1,-3,4,-1,2,1,-5,4]
+[-2,1,-3,4,-1,2,1,-5,4]
+
 current = -2
-maximum = -2
-for i in range of numbers:
-    eg : current = -2, nums[i] = 1
-    therefore = nums[i] = 1, current + nums[i] = -2+1 = -1
-    max(1, -1) = 1 
-    therefore :current = 1
-    last line to compare all values and find out the maximum
+maxsum = nums[0]  => -2
+currentSum = max(nums[i], currentSum + nums[i])
+
+suppose: current = -2, nums[i] = 1
+therefore: nums[i] = 1
+current + nums[i] = -2 + 1 = -1
+max(1, -1) = 1
+current = 1
+
+iteratively find out the other numbers 
 
 """
