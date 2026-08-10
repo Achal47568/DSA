39029b15b2bf68cad5a1f0725e4e34f48d9b0791
Achal47568 <114794524+Achal47568@users.noreply.github.com>
2026-08-10 21:48:55 +0530

    Time: 49 ms (16.42%) | Memory: 31.4 MB (43.78%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -7,3 +7,16 @@ class Solution:
             current = max(nums[i], current+nums[i])
             maximum = max(maximum, current)
         return maximum
+
+"""
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+current = -2
+maximum = -2
+for i in range of numbers:
+    eg : current = -2, nums[i] = 1
+    therefore = nums[i] = 1, current + nums[i] = -2+1 = -1
+    max(1, -1) = 1 
+    therefore :current = 1
+    last line to compare all values and find out the maximum
+
+"""
