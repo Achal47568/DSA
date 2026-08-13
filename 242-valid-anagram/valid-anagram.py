9a33238faebc100e44bf172dca08d9a27e11eeac
Achal47568 <114794524+Achal47568@users.noreply.github.com>
2026-08-13 16:33:34 +0530

    Time: 7 ms (91.65%) | Memory: 19.5 MB (30.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,17 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+    def isAnagram(self, s1: str, s2: str) -> bool:
+        # return Counter(s) == Counter(t)     
+        if len(s1) != len(s2):
+            return False
+        
+        L1 = [0]*128
+        L2 = [0]*128
+
+        for i in s1:
+            L1[ord(i)] +=1
+        for i in s2:
+            L2[ord(i)] +=1
+        if L1 == L2:
+            return True
+        else:
+            return False
