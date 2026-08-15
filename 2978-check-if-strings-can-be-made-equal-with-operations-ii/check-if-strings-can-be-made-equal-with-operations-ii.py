f842f290fe3206c016e896b575b8111d96b3ac19
Achal47568 <114794524+Achal47568@users.noreply.github.com>
2026-08-15 23:20:20 +0530

    Time: 59 ms (83.40%) | Memory: 20.1 MB (56.52%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        even1 = Counter(s1[::2])
+        even2 = Counter(s2[::2])
+
+        odd1 = Counter(s1[1::2])
+        odd2 = Counter(s2[1::2])
+
+        if even1 != even2:
+            return False
+
+        if odd1 != odd2:
+            return False
+
+        return True
